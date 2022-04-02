2765528b850ae4ee24571fa7be2d4e76db4ca495
Danny Rodriguez <danny007111@hotmail.com>
2022-04-02 01:51:34 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+
+const initialValue = 0;
+const totalBatteries = batteryBatches.reduce(
+  (previousValue, currentValue) => previousValue + currentValue,
+  initialValue
+);
+
+// 0 + 4 + 5 + 3 + 4 + 4 + 6 + 5
